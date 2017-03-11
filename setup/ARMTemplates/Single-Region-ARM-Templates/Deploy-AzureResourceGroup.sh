1258bba180a451b0849109d9d4b42ca56f8815d5
Rick Rainey <rickra@microsoft.com>
2017-03-11 10:40:06 -0600

    Adding polling logic to bash script for single-region deployment.

--- setup/ARMTemplates/Single-Region-ARM-Templates/Deploy-AzureResourceGroup.sh
+++ setup/ARMTemplates/Single-Region-ARM-Templates/Deploy-AzureResourceGroup.sh
@@ -114,6 +114,7 @@ do
     az storage blob upload -f $artifact_file -c $STG_CONTAINER_NAME -n $blob_name \
         --account-name $STG_ACCT_NAME --account-key "${STG_ACCT_KEY}"
 done
+echo ""
 
 # Generate a SAS token for the storage container the artifacts were uploaded to
 ARTIFACTS_LOCATION="https://${STG_ACCT_NAME}.blob.core.windows.net/${STG_CONTAINER_NAME}"
@@ -127,22 +128,47 @@ RSRC_GRP_EXISTS=$( az group exists --name ${RSRC_GRP_NAME} )
 if [[ $RSRC_GRP_EXISTS == "false" ]]
 then
     echo "Creating resource group '$RSRC_GRP_NAME' in '$LOCATION'."
-    az group create --location $LOCATION --name $RSRC_GRP_NAME
+    PROVISIONING_STATE=$( az group create --location $LOCATION --name $RSRC_GRP_NAME --query "properties.provisioningState" )
+    PROVISIONING_STATE=${PROVISIONING_STATE//[\"]/}
+    echo "$PROVISIONING_STATE"
 fi
 
+# Build JSON string for parameters to pass to resource group deployment.
 PARAMETERS="{\"_artifactsLocation\": {\"value\": \"${ARTIFACTS_LOCATION}\" }, \"_artifactsLocationSasToken\": {\"value\": \"${ARTIFACTS_LOCATION_SAS_TOKEN}\" } }"
 
 if [[ $VALIDATE_ONLY == true ]]
 then
+    # Validate the ARM templates only - don't deploy.
     echo "Validating resource group deployment..."
     az group deployment validate --resource-group $RSRC_GRP_NAME \
         --template-file $TEMPLATE_FILE --parameters "${PARAMETERS}" --query "properties.provisioningState"
 else
+    # Start the resource group deployment.
     echo "Deploying to resource group... "
     DEPLOYMENT_TIME=$( date -u +%m%d-%H%M )
     DEPLOYMENT_NAME=$(basename "$TEMPLATE_FILE" .json)
     DEPLOYMENT_NAME="$DEPLOYMENT_NAME-$DEPLOYMENT_TIME"
     az group deployment create --resource-group $RSRC_GRP_NAME --name $DEPLOYMENT_NAME \
-         --template-file $TEMPLATE_FILE --parameters "${PARAMETERS}" --query "properties.provisioningState"
+        --template-file $TEMPLATE_FILE --parameters "${PARAMETERS}" --no-wait
+
+    # Color codes used when showing polling output.
+    LIGHT_CYAN='\033[1;36m'
+    NO_COLOR='\033[0m'
+
+    # Poll the deployment status every 10 seconds.
+    INTERVAL=10
+    PROVISIONING_STATE="Checking provisioning state every $INTERVAL seconds"
+    while [ "$PROVISIONING_STATE" != "Succeeded" ] && [ "$PROVISIONING_STATE" != "Failed" ]
+    do
+        DEPLOYMENT_CHECK_TIME=$( date +%H:%m:%S' '%p)
+        echo -e "${LIGHT_CYAN}    $DEPLOYMENT_CHECK_TIME - $PROVISIONING_STATE ... ${NO_COLOR}"
+        sleep $INTERVAL
+        PROVISIONING_STATE=$( az group deployment show --name $DEPLOYMENT_NAME --resource-group $RSRC_GRP_NAME --query "properties.provisioningState" )
+        PROVISIONING_STATE=${PROVISIONING_STATE//[\"]/}
+    done
+
+    echo ""
+    echo "    $PROVISIONING_STATE"
+    echo ""
 fi
 
